18c48c8bce0b0ced8f3fba7293477a907384bdf6
Radhitia Pratama <radhitia43@gmail.com>
2023-11-14 14:01:37 +0700

    final crud

---
+++ TestingCRUD/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace TestingCRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtProduct = new System.Windows.Forms.TextBox();
+            this.txtDesign = new System.Windows.Forms.TextBox();
+            this.txtItem = new System.Windows.Forms.TextBox();
+            this.cbColor = new System.Windows.Forms.ComboBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Update = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(312, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CRUD C#";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(67, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Product ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(67, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Item Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(67, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Design";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(67, 242);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 29);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Color";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtProduct
+            // 
+            this.txtProduct.Location = new System.Drawing.Point(237, 85);
+            this.txtProduct.Name = "txtProduct";
+            this.txtProduct.Size = new System.Drawing.Size(370, 22);
+            this.txtProduct.TabIndex = 5;
+            // 
+            // txtDesign
+            // 
+            this.txtDesign.Location = new System.Drawing.Point(237, 196);
+            this.txtDesign.Name = "txtDesign";
+            this.txtDesign.Size = new System.Drawing.Size(370, 22);
+            this.txtDesign.TabIndex = 6;
+            this.txtDesign.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtItem
+            // 
+            this.txtItem.Location = new System.Drawing.Point(237, 142);
+            this.txtItem.Name = "txtItem";
+            this.txtItem.Size = new System.Drawing.Size(370, 22);
+            this.txtItem.TabIndex = 7;
+            // 
+            // cbColor
+            // 
+            this.cbColor.FormattingEnabled = true;
+            this.cbColor.Items.AddRange(new object[] {
+            "Red",
+            "Yellow",
+            "Green",
+            "Brown",
+            "Blue",
+            "Sky Blue"});
+            this.cbColor.Location = new System.Drawing.Point(237, 247);
+            this.cbColor.Name = "cbColor";
+            this.cbColor.Size = new System.Drawing.Size(370, 24);
+            this.cbColor.TabIndex = 8;
+            this.cbColor.SelectedIndexChanged += new System.EventHandler(this.cbColor_SelectedIndexChanged);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(237, 298);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(110, 43);
+            this.btnInsert.TabIndex = 9;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(72, 375);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(824, 262);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // Update
+            // 
+            this.Update.Location = new System.Drawing.Point(363, 298);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(106, 43);
+            this.Update.TabIndex = 11;
+            this.Update.Text = "Update";
+            this.Update.UseVisualStyleBackColor = true;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(486, 298);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(121, 43);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(626, 80);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(97, 46);
+            this.btnSearch.TabIndex = 13;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(990, 675);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.cbColor);
+            this.Controls.Add(this.txtItem);
+            this.Controls.Add(this.txtDesign);
+            this.Controls.Add(this.txtProduct);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtProduct;
+        private System.Windows.Forms.TextBox txtDesign;
+        private System.Windows.Forms.TextBox txtItem;
+        private System.Windows.Forms.ComboBox cbColor;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ TestingCRUD/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestingCRUD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbColor.DropDownStyle = ComboBoxStyle.DropDownList;
+            bindData();
+        }
+
+        SqlConnection con = new SqlConnection("Data Source=MYLAPTOP\\SQLEXPRESS;Initial Catalog=ProgrammingTutorialDB;Integrated Security=True");
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+
+            con.Open();
+
+            SqlCommand sqlCommand = new SqlCommand("INSERT into ProductInfo_Tab VALUES ('" + int.Parse(txtProduct.Text) + "','" + txtItem.Text + "','" + txtDesign.Text + "','" + cbColor.Text + "',GETDATE(),null)", con);
+
+            sqlCommand.ExecuteNonQuery();
+
+            con.Close();
+
+            MessageBox.Show("Successfully insert data");
+
+            clearTxtBox();
+            bindData();
+        }
+
+        public void clearTxtBox()
+        {
+            txtProduct.Text = "";
+            txtItem.Text = "";
+            txtDesign.Text = "";
+            cbColor.SelectedItem = null;
+        }
+
+        private void cbColor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cbColor.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        public void bindData()
+        {
+            SqlCommand sqlcommand = new SqlCommand("SELECT * FROM ProductInfo_Tab",con);
+            SqlDataAdapter sd = new SqlDataAdapter(sqlcommand);
+            DataTable dt = new DataTable();
+            sd.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            con.Open();
+
+            SqlCommand command = new SqlCommand("UPDATE ProductInfo_Tab SET ItemName='" + txtItem.Text + "',Design='" + txtDesign.Text + "',Color='" + txtDesign.Text + "',UpdateDate = GETDATE() WHERE ProductId = '" + int.Parse(txtProduct.Text) + "'", con);
+
+            command.ExecuteNonQuery();
+
+            con.Close();
+
+            bindData();
+            MessageBox.Show("Product Updated Successfully");
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if(txtProduct.Text != "")
+            {
+                con.Open();
+
+                SqlCommand command = new SqlCommand("DELETE FROM ProductInfo_Tab WHERE ProductID = '" + int.Parse(txtProduct.Text) + "'",con);
+                command.ExecuteNonQuery();
+
+                con.Close();
+
+                bindData();
+
+                MessageBox.Show("Successfully deleted data");
+                return;
+            }
+
+            MessageBox.Show("Product ID wajib di isi");
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            con.Open();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM ProductInfo_Tab WHERE ProductID = '" + txtProduct.Text + "'", con);
+            SqlDataAdapter sd = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            sd.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            con.Close();
+        }
+    }
+}
